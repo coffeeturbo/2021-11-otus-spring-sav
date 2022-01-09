ce79c0b9aaef84522e6eafeb2d214dbbd8a62a31
vsavushkin <v.savushkin@qvant.ru>
2022-01-09 18:13:33 +0530

    up test coverage

--- spring-homework-01/src/main/java/ru/otus/spring/Application.java
+++ spring-homework-01/src/main/java/ru/otus/spring/Application.java
@@ -2,10 +2,14 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.otus.spring.service.QuizService;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        var testingService = context.getBean(QuizService.class);
+        testingService.startQuiz();
     }
 }
